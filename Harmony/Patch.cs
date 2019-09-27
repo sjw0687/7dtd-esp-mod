a01e0a28c261caf16d7949ab2cec373a5499e435
Jaewon Song <sjw0687@gmail.com>
2019-09-27 13:13:15 +0900

    초기 커밋.

---
+++ Harmony/Patch.cs
@@ -0,0 +1,338 @@
+﻿using DMT;
+using Harmony;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace SdtdEsp
+{
+    public class Patch : MonoBehaviour
+    {
+        public const float distForCountingEnemies = 120f;
+        public const float INF = 1234567890f;
+        public const string defaultMsg = "ENEMY INDICATOR ENABLED";
+
+        static GameObject go;
+        static Shader wallhackShader;
+        static Shader normalShader;
+        static Entity player;
+        static AIDirectorZombieManagementComponent aiDirector;
+        public static ESP esp;
+        static Sprite[] sprites;
+
+        //static int zombieCnt = 0;
+        //static double nearestDist = INF;
+
+        static bool AreSpritesLoaded()
+        {
+            foreach (Sprite sprite in sprites)
+            {
+                if (sprite == null)
+                    return false;
+            }
+            return true;
+        }
+
+        static void ReplaceShaders(GameObject gameObject, Shader shader)
+        {
+            var renderer = gameObject.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                foreach (var material in renderer.materials)
+                {
+                    if (normalShader == null)
+                    {
+                        // It's not clear which shader would be chosen for recovery.
+                        // And only one shader is used for all materials.
+                        normalShader = material.shader;
+                    }
+                    material.shader = shader;
+                    material.SetColor("_FirstOutlineColor", Color.red);
+                    material.SetFloat("_FirstOutlineWidth", 0.015f);
+                }
+            }
+            foreach (Transform transform in gameObject.transform)
+            {
+                ReplaceShaders(transform.gameObject, shader);
+            }
+        }
+
+        static bool NameContains(String str, String key)
+        {
+            // TODO returns true only if the string contains a exact word
+            return str.IndexOf(key, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        static EnemyIcon GetIconNum(EntityEnemy enemy)
+        {
+            if (enemy is EntityFlying)
+                return EnemyIcon.Flying;
+            if (enemy is EntityZombieDog || NameContains(enemy.EntityName, "wolf"))
+                return EnemyIcon.Dog;
+            if (NameContains(enemy.EntityName, "bear"))
+                return EnemyIcon.Bear;
+            if (enemy is EntityEnemyAnimal || NameContains(enemy.EntityName, "snake"))
+                return EnemyIcon.Animal;
+            return EnemyIcon.Zombie;
+        }
+
+        static bool IsRunning(EntityEnemy enemy)
+        {
+            EnemyIcon iconNum;
+            if (enemy.IsRunning)
+                return true;
+            iconNum = GetIconNum(enemy);
+            if (iconNum == EnemyIcon.Dog || iconNum == EnemyIcon.Bear)
+                return true;
+            return false;
+        }
+
+        static bool IsFlying(EntityEnemy enemy)
+        {
+            return (enemy is EntityFlying);
+        }
+
+        void Start()
+        {
+            //string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string modPath = Path.Combine(assemblyPath, @"..\..");
+            string resPath = Path.Combine(modPath, "Resources");
+            string bundlePath = Path.Combine(resPath, "esp.unity3d");
+            Debug.Log("bundlePath: " + bundlePath);
+
+            var bundle = AssetBundle.LoadFromFile(bundlePath);
+            wallhackShader = bundle.LoadAsset<Shader>("WallhackShader.shader");
+            sprites = new Sprite[(int)EnemyIcon.ItemCount];
+            sprites[(int)EnemyIcon.Zombie] = bundle.LoadAsset<Sprite>("Zombie.png");
+            sprites[(int)EnemyIcon.Flying] = bundle.LoadAsset<Sprite>("Flying.png");
+            sprites[(int)EnemyIcon.Dog] = bundle.LoadAsset<Sprite>("Dog.png");
+            sprites[(int)EnemyIcon.Bear] = bundle.LoadAsset<Sprite>("Bear.png");
+            sprites[(int)EnemyIcon.Animal] = bundle.LoadAsset<Sprite>("Animal.png");
+            sprites[(int)EnemyIcon.Arrow] = bundle.LoadAsset<Sprite>("Arrow.png");
+
+            esp = new ESP();
+            esp.Init();
+            esp.UpdateText(defaultMsg, Color.white);
+        }
+
+        public class Patch_Init : IHarmony
+        {
+            public void Start()
+            {
+                go = new GameObject();
+                go.AddComponent<Patch>();
+                DontDestroyOnLoad(go);
+
+                Debug.Log(" Loading Patch: " + GetType().ToString());
+                var harmony = HarmonyInstance.Create(GetType().ToString());
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+        }
+
+        [HarmonyPatch(typeof(AIDirectorZombieManagementComponent))]
+        [HarmonyPatch("Tick")]
+        [HarmonyPatch(new Type[] { typeof(double) })]
+        public class PatchUpdateEnemies
+        {
+            static void Postfix(AIDirectorZombieManagementComponent __instance, double _dt)
+            {
+                aiDirector = __instance;
+                var zombies = __instance.trackedZombies;
+                int zombieCnt = 0;
+                float nearestDist = INF;
+                int attackingEnemeies = 0;
+                int investigatingEnemies = 0;
+                bool hasRunner = false;
+                bool hasFlyer = false;
+                string uiStr = "NO ENEMIES";
+                Color OrangeColor = new Color(1.0f, 0.647f, 0f);
+                Color uiStrColor = Color.green;
+
+                // Reset indicator targets before every updates
+                if (esp != null)
+                    esp.targets.Clear();
+
+                for (int i = 0; i < zombies.Count; i++)
+                {
+                    AIDirectorZombieState zs = zombies.list[i];
+                    float dist = INF;
+                    bool isSleeping;
+                    bool isAttacking = false;
+                    bool isInvestigating = false;
+                    Color targetColor;
+                    EnemyIcon iconNum;
+
+                    if (!zs.Zombie.IsAlive())
+                        continue;
+                    if (player != null)
+                    {
+                        dist = zs.Zombie.GetDistance(player);
+                    }
+
+                    if (dist > distForCountingEnemies)
+                        continue;
+                    if (dist < nearestDist)
+                    {
+                        nearestDist = dist;
+                    }
+
+                    // Set enemy state
+                    if (zs.Zombie.GetAttackTarget() == player)
+                        isAttacking = true;
+                    if (zs.Zombie.HasInvestigatePosition)
+                        isInvestigating = true;
+                    isSleeping = zs.Zombie.IsSleeping;
+
+                    // Set target color
+                    if (isAttacking)
+                        targetColor = Color.red;
+                    else if (isInvestigating)
+                        targetColor = Color.yellow;
+                    else if (isSleeping)
+                        targetColor = Color.gray;
+                    else
+                        targetColor = Color.white;
+
+                    if (isAttacking)
+                        attackingEnemeies++;
+                    if (isInvestigating)
+                        investigatingEnemies++;
+
+                    // Is running or flying?
+                    if (isAttacking || isInvestigating)
+                    {
+                        if (IsFlying(zs.Zombie))
+                            hasFlyer = true;
+                        if (IsRunning(zs.Zombie))
+                            hasRunner = true;
+                    }
+
+                    // Add target to indicator
+                    int zId = zs.Zombie.GetInstanceID();
+                    if (esp != null && !esp.targets.ContainsKey(zId))
+                    {
+
+                        iconNum = GetIconNum(zs.Zombie);
+                        esp.targets.Add(
+                            zId,
+                            new EnemyInfo(
+                                zs.Zombie.gameObject,
+                                targetColor,
+                                sprites[(int)iconNum],
+                                iconNum == EnemyIcon.Arrow));
+                    }
+
+                    zombieCnt++;
+                }
+
+                // Update indicator
+                if(esp != null)
+                    esp.DoUpdate(Camera.main);
+                if (zombieCnt > 0 && nearestDist < INF)
+                {
+                    uiStr = zombieCnt + " ENEMIES" + "  (" + nearestDist.ToString("0.00") + "M)";
+                    uiStr += "\n";
+
+                    if (attackingEnemeies > 0)
+                    {
+                        uiStr += attackingEnemeies + " ATTACKING";
+                        uiStrColor = Color.red;
+                    }
+                    else if (investigatingEnemies > 0)
+                    {
+                        uiStr += investigatingEnemies + " INVESTIGATING";
+                        uiStrColor = Color.yellow;
+                    }
+                    else
+                    {
+                        uiStr += "IDLE";
+                        uiStrColor = Color.white;
+                    }
+
+                    if (hasRunner && hasFlyer)
+                    {
+                        uiStr += "\nRUN / FLY";
+                    }
+                    else if (hasFlyer)
+                    {
+                        uiStr += "\nFLY";
+                    }
+                    else if (hasRunner)
+                    {
+                        uiStr += "\nRUN";
+                    }
+                }
+                else if (player == null)
+                {
+                    uiStr = "No player found. Exit to main menu and reload the game.";
+                    uiStrColor = Color.white;
+                }
+                if (esp != null)
+                    esp.UpdateText(uiStr, uiStrColor);
+            }
+        }
+
+        [HarmonyPatch(typeof(AIDirectorZombieManagementComponent))]
+        [HarmonyPatch("AddZombie")]
+        [HarmonyPatch(new Type[] { typeof(EntityEnemy) })]
+        public class PatchReplaceEnemyShader
+        {
+            static void Postfix(EntityEnemy _zombie)
+            {
+                if (wallhackShader != null)
+                {
+                    ReplaceShaders(_zombie.gameObject, wallhackShader);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(World))]
+        [HarmonyPatch("SetLocalPlayer")]
+        [HarmonyPatch(new Type[] { typeof(EntityPlayerLocal) })]
+        public class PatchGetPlayer
+        {
+            static void Postfix(World __instance)
+            {
+                player = __instance.GetPrimaryPlayer();
+            }
+        }
+
+        [HarmonyPatch(typeof(EntityAlive))]
+        [HarmonyPatch("OnEntityDeath")]
+        [HarmonyPatch(new Type[] { })]
+        public class PatchRecoverEnemyShader
+        {
+            static void Postfix(EntityAlive __instance)
+            {
+                int entityId = __instance.entityId;
+                var zombies = (aiDirector != null) ? aiDirector.trackedZombies : null;
+                if (zombies != null)
+                {
+                    if (zombies.dict.ContainsKey(entityId))
+                    {
+                        if (normalShader != null)
+                            ReplaceShaders(__instance.gameObject, normalShader);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(World))]
+        [HarmonyPatch("Cleanup")]
+        [HarmonyPatch(new Type[] { })]
+        public class PatchCleanUp
+        {
+            static void Postfix()
+            {
+                aiDirector = null;
+                if (esp != null)
+                    esp.DoUpdate(null); // Clear targets
+                player = null;
+                if (esp != null)
+                    esp.UpdateText(defaultMsg, Color.white);
+            }
+        }
+    }
+}
---
+++ Scripts/ESP.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SdtdEsp
+{
+
+    public enum EnemyIcon
+    {
+        Zombie,
+        Flying,
+        Dog,
+        Bear,
+        Animal,
+        Arrow,
+        ItemCount,
+    }
+
+    public struct EnemyInfo
+    {
+        public GameObject gameObject;
+        public Color color;
+        public Sprite icon;
+        public bool applyRotation;
+
+        public EnemyInfo(GameObject _gameObject, Color _color, Sprite _icon, bool _applyRotation)
+        {
+            gameObject = _gameObject;
+            color = _color;
+            icon = _icon;
+            applyRotation = _applyRotation;
+        }
+    }
+
+    public class ESP
+    {
+        public const float targetColorAlpha = 0.65f;
+        static float targetEdgeDistance = 1.0f;
+        static float indicatorEdgeDistance = 0.925f;
+        static float maxTargetDistance = 50f;
+
+        public Dictionary<int, EnemyInfo> targets = new Dictionary<int, EnemyInfo>();
+        Dictionary<int, GameObject> panels = new Dictionary<int, GameObject>();
+        GameObject canvasObj;
+        Text text;
+
+        public void Init()
+        {
+            CreateCanvas();
+
+            GameObject textObj = new GameObject();
+
+            text = textObj.AddComponent<Text>();
+            text.font = ((Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf"));
+            text.fontSize = 20;
+            text.color = Color.white;
+            text.alignment = TextAnchor.MiddleCenter;
+            Outline outline = textObj.AddComponent<Outline>();
+            outline.effectColor = Color.black;
+
+            textObj.transform.SetParent(canvasObj.transform);
+            textObj.SetActive(true);
+            float textWidth = 350f;
+            float textHeight = 80f;
+            text.transform.position = new Vector3(
+                50f + textWidth / 2f,
+                (Screen.height - 25f) - textHeight / 2f,
+                0);
+            text.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, textWidth);
+            text.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight);
+        }
+
+        public void UpdateText(string str, Color color)
+        {
+            text.text = str;
+            text.color = color;
+        }
+
+        public void DoUpdate(Camera mainCamera)
+        {
+            if (mainCamera == null)
+                targets.Clear();
+
+            HashSet<int> updatedIDs = new HashSet<int>();
+            foreach (KeyValuePair<int, EnemyInfo> pair in targets)
+            {
+                GameObject target = pair.Value.gameObject;
+                //  Get the target's world position in screen coordinate position
+                Vector3 targetPosOnScreen = mainCamera.WorldToScreenPoint(target.transform.position);
+                if (OnScreen(targetPosOnScreen, mainCamera))
+                    continue;
+                float distanceFromViewer = GetDistance(target.transform.position, mainCamera.transform.position);
+                if (distanceFromViewer > maxTargetDistance)
+                    continue;
+                UpdateOffScreen(
+                    pair.Key,
+                    targetPosOnScreen,
+                    distanceFromViewer,
+                    pair.Value.color,
+                    pair.Value.icon,
+                    pair.Value.applyRotation);
+                updatedIDs.Add(pair.Key);
+            }
+            List<int> panelsToRemove = new List<int>();
+            foreach (KeyValuePair<int, GameObject> panel in panels)
+            {
+                if (updatedIDs.Contains(panel.Key))
+                    continue;
+                UnityEngine.Object.Destroy(panel.Value);
+                panelsToRemove.Add(panel.Key);
+            }
+            foreach (int idToRemove in panelsToRemove)
+            {
+                panels.Remove(idToRemove);
+            }
+        }
+
+        void UpdateOffScreen(int id, Vector3 targetPosOnScreen, float distance, Color color, Sprite sprite, bool applyRotation)
+        {
+            //  Create a variable for the center position of the screen.
+            Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) / 2;
+
+            //  Set newIndicatorPos anchor to the center of the screen instead of bottom left
+            Vector3 newIndicatorPos = targetPosOnScreen - screenCenter;
+
+            //  Flip the newIndicatorPos to correct the calculations for indicators behind the camera.
+            if (newIndicatorPos.z < 0)
+                newIndicatorPos *= -1;
+
+            //  Get angle from center of screen to target position
+            float angle = Mathf.Atan2(newIndicatorPos.y, newIndicatorPos.x);
+            angle -= 90 * Mathf.Deg2Rad;
+
+            //  y = mx + b (intercept forumla)
+            float cos = Mathf.Cos(angle);
+            float sin = Mathf.Sin(angle);
+            float m = cos / sin;
+
+            //  Create the screen boundaries that the indicators reside in.
+            Vector3 screenBounds = new Vector3(screenCenter.x * indicatorEdgeDistance, screenCenter.y * indicatorEdgeDistance);
+
+            //  Check which screen side the target is currently in.
+            //  Check top & bottom
+            if (cos > 0)
+                newIndicatorPos = new Vector2(-screenBounds.y / m, screenBounds.y);
+            else
+                newIndicatorPos = new Vector2(screenBounds.y / m, -screenBounds.y);
+
+            //  Check left & right
+            if (newIndicatorPos.x > screenBounds.x)
+                newIndicatorPos = new Vector2(screenBounds.x, -screenBounds.x * m);
+            else if (newIndicatorPos.x < -screenBounds.x)
+                newIndicatorPos = new Vector2(-screenBounds.x, screenBounds.x * m);
+
+            //  Reset the newIndicatorPos anchor back to bottom left corner.
+            newIndicatorPos += screenCenter;
+
+            Image image;
+
+            if (!panels.ContainsKey(id))
+            {
+                GameObject panel = new GameObject();
+
+                image = panel.AddComponent<Image>();
+                image.sprite = sprite;
+
+                Outline outline = panel.AddComponent<Outline>();
+                Color outlineColor = Color.black;
+                outlineColor.a = targetColorAlpha;
+                outline.effectColor = outlineColor;
+                outline.effectDistance = new Vector2(4f, 4f);
+
+                panel.GetComponent<RectTransform>().SetParent(canvasObj.transform);
+                panel.SetActive(true);
+                panels.Add(id, panel);
+            }
+            else
+            {
+                image = panels[id].GetComponent<Image>();
+            }
+
+            color.a = targetColorAlpha;
+            image.color = color;
+
+            //  Assign new position
+            panels[id].transform.position = new Vector3(newIndicatorPos.x, newIndicatorPos.y, targetPosOnScreen.z);
+
+            // Set the scale
+            float scaleDenom = 0.05f * distance + 1.055f;
+            scaleDenom = scaleDenom * scaleDenom;
+            float newScale = 1f / scaleDenom * 1.111f;
+            panels[id].transform.localScale = new Vector2(newScale, newScale);
+
+            //  Assign new rotation
+            if (applyRotation)
+                panels[id].transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        }
+
+        float GetDistance(Vector3 PosA, Vector3 PosB)
+        {
+            Vector3 heading;
+
+            //  Subtracting from both vectors returns the magnitude
+            heading.x = PosA.x - PosB.x;
+            heading.y = PosA.y - PosB.y;
+            heading.z = PosA.z - PosB.z;
+
+            //  Return the sqaure root of the sum of each sqaured vector axises.
+            return Mathf.Sqrt((heading.x * heading.x) + (heading.y * heading.y) + (heading.z * heading.z));
+        }
+
+        void CreateCanvas()
+        {
+            //  Create gameobject that holds canvas
+            canvasObj = new GameObject("Enemy_Indicator_Canvas");
+
+            //  Create Canvas
+            Canvas canvas = canvasObj.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = -100;
+
+            //  Create default components for canvas
+            CanvasScaler cs = canvasObj.AddComponent<CanvasScaler>();
+            cs.scaleFactor = 1;
+            cs.dynamicPixelsPerUnit = 10;
+            canvasObj.AddComponent<GraphicRaycaster>();
+        }
+
+        bool OnScreen(Vector3 pos, Camera mainCamera)
+        {
+            if (pos.x < (Screen.width * targetEdgeDistance) && pos.x > (Screen.width - Screen.width * targetEdgeDistance) &&
+                pos.y < (Screen.height * targetEdgeDistance) && pos.y > (Screen.height - Screen.height * targetEdgeDistance) &&
+                pos.z > mainCamera.nearClipPlane && pos.z < mainCamera.farClipPlane)
+                return true;
+            return false;
+        }
+    }
+
+}
